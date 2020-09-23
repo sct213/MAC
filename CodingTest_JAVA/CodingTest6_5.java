d602f6031a5d05af407a260d8f1b7c0a8af624c7
sct213 <yang991211@naver.com>
2020-09-23 17:34:47 +0900

    test

--- CodingTest_JAVA/CodingTest6_5.java
+++ CodingTest_JAVA/CodingTest6_5.java
@@ -1,34 +1,23 @@
-package Personal;
-import java.util.*;
+
+import java.util.ArrayList;
 public class CodingTest6_5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		ArrayList ar = new ArrayList();
 		
-		ar.add("객체지향 프로그래밍");
+		ArrayList ar = new ArrayList<>();
+		
+		ar.add("객체지향프로그래밍");
 		ar.add("하둡");
 		ar.add("MongoDB");
 		
-		int i = 1;
-		System.out.println("<" + ar.get(0) + ">");
-		while(i<ar.size()) {
-			System.out.println(ar.get(i));
+		int i = 0;
+		while(i<ar.size()){
+			if(i==0){
+				System.out.println("<" + ar.get(i) + ">");
+			} else {
+				System.out.println(ar.get(i));
+			}
 			i++;
 		}
-//		int a = 1;
-		
-//		Iterator it = ar.iterator();
-//		
-//		while(it.hasNext()) {
-//			
-//			System.out.println(it.next());
-//			if(a==1) {
-//				System.out.println("<" + it.next() + ">");
-//			}else {
-//				System.out.println(it);
-//			}
-		
 	}
-
 }
